d6df97b54536a9abf1d2ac6ee7facfe3540857d0
kilocharlie96 <cernak.kiko@gmail.com>
2024-03-20 20:21:54 +0100

    Opravena cesta k partials. CSS funguje.

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include 'partials/header.php'; 
+?>
+		<article>
+			<header class="post-header">
+				<div class="container">
+					<h1 class="post-title">Portfólio našich prác</h1>
+				</div>
+			</header>
+			<div class="post-content">
+				<div class="container">
+
+					<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
+
+					<ul class="menu controls">
+						<li data-from="left" class="selected"><a href="#vsetko">Všetko</a></li>
+						<li data-from="right"><a href="partials/web.html">Web</a></li>
+						<li data-from="left"><a href="partials/branding.html">Branding</a></li>
+						<li data-from="right"><a href="partials/fotografia.html">Fotografia</a></li>
+						<li data-from="left"><a href="partials/video.html">Video</a></li>
+					</ul>
+
+				</div>
+
+				<section class="gallery">
+					<div class="gallery-set container" id="vsetko">
+						<a href="assets/img/samurai/image-1.jpg">
+							<img src="assets/img/samurai/thumb-1.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="assets/img/samurai/image-2.jpg">
+							<img src="assets/img/samurai/thumb-2.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="assets/img/samurai/image-3.jpg">
+							<img src="assets/img/samurai/thumb-3.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="assets/img/samurai/image-4.jpg">
+							<img src="assets/img/samurai/thumb-4.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="assets/img/samurai/image-5.jpg">
+							<img src="assets/img/samurai/thumb-5.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+						<a href="assets/img/samurai/image-6.jpg">
+							<img src="assets/img/samurai/thumb-6.jpg" height="192" width="383" alt="jackie boy">
+						</a>
+					</div>
+				</section>
+			</div>
+		</article>
+	</main>
+
+	<aside class="pre-footer">
+		<div class="container">
+			<p>
+				<strong>Zaujali sme Vás pre nový projekt?</strong>
+				Jednoducho nám zavolajte alebo napíšte.
+			</p>
+
+			<ul class="menu personal">
+				<li><i class="fa fa-phone"></i><a href="tel:+421900111222">+421 900 111 222</a></li>
+				<li><i class="fa fa-envelope"></i><a href="mailto:email@mailinator.com">email@mailinator.com</a></li>
+			</ul>
+		</div>
+	</aside>
+
+	<?php include 'partials/footer.php'; ?>
+
+	<!-- scripts -->
+	<script src="assets/js/lightbox.js"></script>
+
+</body>
+</html>
