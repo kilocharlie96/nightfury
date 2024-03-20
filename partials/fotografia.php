16b0c1d8c8233b455c96bd7bf9ee6c20e913ad38
kilocharlie96 <cernak.kiko@gmail.com>
2024-03-20 22:10:36 +0100

    Z fotografie už ide prepnuť na branding.

--- partials/fotografia.php
+++ partials/fotografia.php
@@ -14,7 +14,7 @@
 					<ul class="menu controls">
 						<li data-from="left"><a href="../index.php">Všetko</a></li>
 						<li data-from="right"><a href="web.php">Web</a></li>
-						<li data-from="left"><a href="#branding">Branding</a></li>
+						<li data-from="left"><a href="branding.php">Branding</a></li>
 						<li data-from="right" class="selected"><a href="#fotografia">Fotografia</a></li>
 						<li data-from="left"><a href="video.php">Video</a></li>
 					</ul>
