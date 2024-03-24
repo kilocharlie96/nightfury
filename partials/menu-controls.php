ad865e4a63b0c9f947b65551328f4cf844869696
kilocharlie96 <cernak.kiko@gmail.com>
2024-03-24 19:25:58 +0100

    Menu pre portfólio zautomatizované podľa .php stránok. Funguje vyznačenie aktuálnej stránky na ktorej som.

--- partials/menu-controls.php
+++ partials/menu-controls.php
@@ -4,22 +4,20 @@
 
 	foreach ( $pages as $file ){
 		$page = basename($file, '.php');
-        if($page == 'index'){
-            $page = 'všetko';
-        }
 
         if($page_name == $page){
+            if($page == 'index'){
+                $page = 'všetko';
+            }
             echo '<li class="selected"><a href="' .$file. '">' .$page. '</a></li>';
         } else {
+            if($page == 'index'){
+                $page = 'všetko';
+            }
             echo '<li><a href="' .$file. '">' .$page. '</a></li>';
         }
 	}
-
+//todo skúsiť vynechať jeden if v rámci DRY metodiky
 ?>
 
-    <!-- <li data-from="left"><a href="index.php">Všetko</a></li>
-    <li data-from="right"><a href="web.php">Web</a></li>
-    <li data-from="left"><a href="branding.php">Branding</a></li>
-    <li data-from="right"><a href="fotografia.php">Fotografia</a></li>
-    <li data-from="left"><a href="video.php">Video</a></li> -->
 </ul>
--- partials/portfolio.php
+++ partials/portfolio.php
@@ -14,14 +14,6 @@
 			<?php 
 				include 'partials/menu-controls.php';
 			?>
-
-			<!-- <ul class="menu controls">
-				<li data-from="left"><a href="index.php">Všetko</a></li>
-				<li data-from="right"><a href="web.php">Web</a></li>
-				<li data-from="left"><a href="branding.php">Branding</a></li>
-				<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
-				<li data-from="left"><a href="video.php">Video</a></li>
-			</ul> -->
 			
 		</div>
 
