f6d02d11d0141e37c4ae324db4b3b2af50d6fc1f
kilocharlie96 <cernak.kiko@gmail.com>
2024-03-24 15:21:49 +0100

    Vytvorený php súbor pre menu v portfóliu a vložený pomocou include

---
+++ partials/menu-controls.php
@@ -0,0 +1,7 @@
+<ul class="menu controls">
+    <li data-from="left"><a href="index.php">Všetko</a></li>
+    <li data-from="right"><a href="web.php">Web</a></li>
+    <li data-from="left"><a href="branding.php">Branding</a></li>
+    <li data-from="right"><a href="fotografia.php">Fotografia</a></li>
+    <li data-from="left"><a href="video.php">Video</a></li>
+</ul>
--- partials/portfolio.php
+++ partials/portfolio.php
@@ -1,25 +1,27 @@
 <article>
-			<header class="post-header">
-				<div class="container">
-					<h1 class="post-title">Portfólio našich prác</h1>
-				</div>
-			</header>
-			<div class="post-content">
-				<div class="container">
+	<header class="post-header">
+		<div class="container">
+			<h1 class="post-title">Portfólio našich prác</h1>
+		</div>
+	</header>
+	<div class="post-content">
+		<div class="container">
 
-					<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
+			<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
 
+			<?php 
+				include 'partials/menu-controls.php';
+			?>
 
-
-					<ul class="menu controls">
-						<li data-from="left"><a href="index.php">Všetko</a></li>
-						<li data-from="right"><a href="web.php">Web</a></li>
-						<li data-from="left"><a href="branding.php">Branding</a></li>
-						<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
-						<li data-from="left"><a href="video.php">Video</a></li>
-					</ul>
-
-				</div>
+			<!-- <ul class="menu controls">
+				<li data-from="left"><a href="index.php">Všetko</a></li>
+				<li data-from="right"><a href="web.php">Web</a></li>
+				<li data-from="left"><a href="branding.php">Branding</a></li>
+				<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
+				<li data-from="left"><a href="video.php">Video</a></li>
+			</ul> -->
+			
+		</div>
 	<?php
 
 	$pages = glob('*.php');
